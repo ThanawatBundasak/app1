0346086ce151d61b9dd25151a1411e3b2f3bb18e
ThanawatBundasak <s6303051633018@kmutnb.ac.th>
2023-08-18 13:51:45 +0700

    add RandomBanner
    add RandomBanner

--- src/App.js
+++ src/App.js
@@ -1,6 +1,7 @@
 import React from 'react';
 import { Header,Content,Footer} from './func-components'
 import Calendar from './class-components'
+import Banner from './banner'
 function App() {
 
   return (
@@ -8,9 +9,11 @@ function App() {
       <Header/>
       <p><center><Calendar/></center></p>
       <Content/>
+      <Banner/>
       <Footer/>
     </>
   );
 }
 
 export default App;
+
