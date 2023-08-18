8a59c147f10b0d208a7365434b4c36a5951a36ce
ThanawatBundasak <s6303051633018@kmutnb.ac.th>
2023-08-18 15:21:16 +0700

    edit App.js
    change App.js for show EventData2

--- src/App.js
+++ src/App.js
@@ -1,25 +1,7 @@
 import React from 'react';
-import { Header,Content,Footer} from './func-components'
-import Calendar from './class-components'
-import Button from './class-components2'
-import {Calculator2} from './calculator'
 import {EventData2} from './event-data'
-import Banner from './banner'
-function App() {
 
-  return (
-    <>
-      <Header/>
-      <p><center><Calendar/></center></p>
-      <Content/>
-      <Banner/>
-      <Calculator2/>
-      <EventData2/>
-      <Button/>
-      <Footer/>
-    </>
-  );
+export default function App(){
+  return <EventData2/>
 }
-
-export default App;
 
