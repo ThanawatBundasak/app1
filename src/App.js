40cbff4023e439093f55939ce985c6a019ca7493
ThanawatBundasak <s6303051633018@kmutnb.ac.th>
2023-08-18 14:56:11 +0700

    add button and calculator
    add class-component2.js
    add calculator.js

--- src/App.js
+++ src/App.js
@@ -1,6 +1,8 @@
 import React from 'react';
 import { Header,Content,Footer} from './func-components'
 import Calendar from './class-components'
+import Button from './class-components2'
+import {Calculator2} from './calculator'
 import Banner from './banner'
 function App() {
 
@@ -10,6 +12,8 @@ function App() {
       <p><center><Calendar/></center></p>
       <Content/>
       <Banner/>
+      <Calculator2/>
+      <Button/>
       <Footer/>
     </>
   );
