ad29ff32b5fc490116fd5f805969dcbde9f01603
ThanawatBundasak <s6303051633018@kmutnb.ac.th>
2023-09-22 15:31:06 +0700

    router

--- src/router4.js
+++ src/router4.js
@@ -1,5 +1,5 @@
 import React from "react";
-import { BrowserRouter, NavLink, Navigate, Route, Routes} from "react-router-dom";
+import {Navigate, Route, Routes} from "react-router-dom";
 
 export default function Router4(){
     return(
